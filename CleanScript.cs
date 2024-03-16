0fa4b51b8b7c90089dabca799b1d85ecb9e8ec86
Maxim <83632765+Maksibi@users.noreply.github.com>
2024-03-16 16:35:48 +0700

    Add files via upload

---
+++ CleanScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CodeBase.Editor
+{
+    public class FindMissingScriptsRecursivelyAndRemove : EditorWindow
+    {
+        [MenuItem("Auto/Remove Missing Scripts Recursively")]
+        private static void FindAndRemoveMissingInSelected()
+        {
+            var deepSelection = EditorUtility.CollectDeepHierarchy(Selection.gameObjects);
+            var compCount = 0;
+            var goCount = 0;
+            
+            foreach (var o in deepSelection)
+            {
+                if (o is not GameObject go) 
+                    continue;
+                
+                var count = GameObjectUtility.GetMonoBehavioursWithMissingScriptCount(go);
+
+                if (count <= 0)
+                    continue;
+                
+                Undo.RegisterCompleteObjectUndo(go, "Remove missing scripts");
+                GameObjectUtility.RemoveMonoBehavioursWithMissingScript(go);
+                compCount += count;
+                goCount++;
+            }
+
+            Debug.Log($"Found and removed {compCount} missing scripts from {goCount} GameObjects");
+        }
+    }
+}
